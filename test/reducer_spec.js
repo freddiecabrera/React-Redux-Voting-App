906a9efc270fe9593c37c0c7deb033789b1de3e0
freddiecabrera <freddiecabrera18@yahoo.com>
2016-04-18 23:13:46 -0700

    create reducer function and test it

---
+++ test/reducer_spec.js
@@ -0,0 +1,42 @@
+import { Map, fromJS } from 'immutable'
+import { expect } from 'chai'
+import reducer from '../src/reducer.js'
+
+describe( 'reducer', () => {
+
+ it('handles SET_ENTRIES', () => {
+  const initialState = Map()
+  const action = { type: 'SET_ENTRIES', entries: ['Friday'] }
+  const nextState = reducer( initialState, action )
+  expect(nextState).to.equal( fromJS({
+   entries: ['Friday']
+  }))
+ })
+
+ it('handles NEXT', () => {
+  const initialState = fromJS({ entries: ['Friday', 'Next Friday'] })
+  const action = { type: 'NEXT' }
+  const nextState = reducer( initialState, action )
+  expect(nextState).to.equal( fromJS({
+   vote: { pair: ['Friday', 'Next Friday'] },
+   entries: []
+  }))
+ })
+
+ it('handles VOTE', () => {
+  const initialState = fromJS({
+   vote: { pair: ['Friday', 'Next Friday'] },
+   entries: []
+  })
+  const action = { type: 'VOTE', entry: 'Friday'}
+  const nextState = reducer( initialState, action )
+  expect(nextState).to.equal( fromJS({
+   vote: {
+    pair: ['Friday', 'Next Friday'],
+    tally: {'Friday': 1}
+   },
+   entries: []
+  }))
+ })
+
+})
